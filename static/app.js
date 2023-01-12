d9ff437418dfa2a90825908b00a39e952aeea16c
poni <poni@macchina.local>
2023-01-12 04:27:14 +0100

    ok

--- static/app.js
+++ static/app.js
@@ -1,4 +1,43 @@
 
+function toggleDiv(bool,button,div){
+    let toOpen = document.getElementById(div)
+    document.getElementById(button).addEventListener('click', function(){
+        
+              if (bool == 0) {
+                toOpen.classList.remove("animate__slideOutUp");
+                toOpen.classList.add("animate__slideInDown");
+                toOpen.style.display = 'block'
+                bool++
+                console.log(bool)
+            } else {
+                toOpen.classList.remove("animate__slideInDown");
+                 toOpen.classList.add("animate__slideOutUp");
+                setTimeout(() => {
+                     toOpen.style.display = 'none'
+                }, 1200);
+                bool--
+                console.log(bool)
+               }
+
+      })
+}
+
+
+let viewportHeight = window.innerHeight;
+document.querySelector('.title').addEventListener('click', function(){
+    window.scrollBy(0,viewportHeight)
+})
+
+
+ 
+
+
+
+
+
+
+let toggleInfo = 0
+toggleDiv(toggleInfo,'infoButton','info')
 
 document.querySelector('#logo').addEventListener('click',function(){
     document.querySelector('#logo').classList.add('animate__heartBeat')
